42326109fe44b3c4aa6fb500373c1da9d73caec9
InnaLeonova <innaleonovav@gmail.com>
2022-01-24 13:49:25 -0500

    Added output User Data

--- loops-in-action/looops-in-action.js
+++ loops-in-action/looops-in-action.js
@@ -24,14 +24,38 @@ calculateSumButtonElement.addEventListener('click', calculateSum);
 
 // Hightlight links
 
-const highlightLinksButtonElement = document.querySelector('#highlight-links button');
+const highlightLinksButtonElement = document.querySelector(
+  '#highlight-links button'
+);
 // console.dir(highlightLinksButtonElement);
 function highlightLinks() {
-    const anchorElements = document.querySelectorAll('#highlight-links a');
-    for (const anchorElement of anchorElements) {
-        anchorElement.classList.add('highlight');
-    }
-
+  const anchorElements = document.querySelectorAll('#highlight-links a');
+  for (const anchorElement of anchorElements) {
+    anchorElement.classList.add('highlight');
+  }
 }
 
 highlightLinksButtonElement.addEventListener('click', highlightLinks);
+
+// Display user data
+
+const userData = {
+  firstName: 'Maria',
+  lastName: 'Ivanova',
+  age: 40,
+};
+
+const displayUserDataButtonElement =
+  document.querySelector('#user-data button');
+
+function displayUserData() {
+  const outputDataElement = document.getElementById('output-user-data');
+  outputDataElement.innerHTML = '';
+  for (const key in userData) {
+    const newUserDataListItemElement = document.createElement('li');
+    const outputText = key.toUpperCase() + ': ' + userData[key];
+    newUserDataListItemElement.textContent = outputText;
+    outputDataElement.append(newUserDataListItemElement);
+  }
+}
+displayUserDataButtonElement.addEventListener('click', displayUserData);
